dba6e98c7d57943a18850ca97ad06279cfca4e97
Andrei Johann <andrei_johann@yahoo.com>
2017-08-24 18:17:56 +1000

    Action Parameters

--- JohannMovies/Controllers/MoviesController.cs
+++ JohannMovies/Controllers/MoviesController.cs
@@ -21,14 +21,29 @@ namespace JohannMovies.Controllers
             /* Playing with Action Results */
             //return Content("Random Method!");
             //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home");
+            //return new EmptyResult();            
             //return Json(Movie, JsonRequestBehavior.AllowGet);
             //return Redirect(@"~\Util\WebFormTest.aspx");
             //return PartialView(@"~\Views\Movies\Random.cshtml", Movie);
-            return File(new System.IO.StreamReader(Server.MapPath("~/Util/Download.docx")).BaseStream, "application/ms-word", "DwnFile.docx");
+            //return File(new System.IO.StreamReader(Server.MapPath("~/Util/Download.docx")).BaseStream, "application/ms-word", "DwnFile.docx");
+            return RedirectToAction("Index", "Home", new { pageIndex = 1, SortBy = "Name" });
+        }
 
+        //GET: Movies/Edit/1 
+        public ActionResult Edit(int id) {
 
+            return Content("id=" + id);
+
+        }
+
+        public ActionResult Index(int? PageIndex, string SortBy) {
+
+            if (!PageIndex.HasValue)
+                PageIndex = 1;
+            if (String.IsNullOrWhiteSpace(SortBy))
+                SortBy = "Name";
+
+            return Content("PageIndex=" + PageIndex + "&SortBy=" + SortBy);
         }
     }
 }
