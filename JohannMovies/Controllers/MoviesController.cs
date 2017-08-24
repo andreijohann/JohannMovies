b6463b623af0108f28526e51a1a52729a9bae4b6
Andrei Johann <andrei_johann@yahoo.com>
2017-08-24 15:50:16 +1000

    Initial Project Load

---
+++ JohannMovies/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JohannMovies.Models;
+
+namespace JohannMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var Movie = new Movie(){
+                Name = "Shrek!"
+            };
+            return View(Movie);
+        }
+    }
+}
