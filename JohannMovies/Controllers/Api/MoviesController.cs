8656a4d664d26de50dc5d2d19f19bf258a864f2b
Andrei Johann <andrei_johann@yahoo.com>
2017-08-29 14:32:39 +1000

    Building Asp.Net WEB-API for Movies

---
+++ JohannMovies/Controllers/Api/MoviesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using JohannMovies.Models;
+using JohannMovies.Dtos;
+using AutoMapper;
+
+namespace JohannMovies.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+
+        //GET /api/movies
+        public IHttpActionResult GetMovies() {
+
+            return Ok(_context.Movies.ToArray().Select(Mapper.Map<Movie, MovieDto>));
+
+        }
+
+        //GET /api/movies/1
+        public IHttpActionResult GetMovie(int Id) {
+
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == Id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+
+        //POST /api/movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto) {
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/api/movies/" + movie.Id), Mapper.Map<Movie, MovieDto>(movie));
+
+        }
+
+        //PUT /api/movie/1
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int Id, MovieDto movieDto) {
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == Id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+
+            Mapper.Map(movieDto, movieInDb);
+            
+            _context.SaveChanges();
+
+            return Ok();
+
+        }
+
+        //DELETE /api/movies/1
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int Id) {
+
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == Id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+
+            return Ok();
+
+        }
+
+
+    }
+}
---
+++ JohannMovies/Dtos/MovieDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace JohannMovies.Models
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        public DateTime? DateAdded { get; set; }
+
+        [Required]
+        [Range(1,20)]
+        public int? NumberInStock { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        
+    }
+
+
+}
